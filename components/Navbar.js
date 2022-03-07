4c9e4d4fc8329999bb52cb16506d3a249d8cac1b
Reza Ageng <rezaageng5@gmail.com>
2022-03-07 19:02:47 +0700

    update navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,8 +5,8 @@ function Navbar() {
   const router = useRouter()
 
   return (
-    <nav className="max-w-full bg-slate-900 text-white">
-      <ul className="flex gap-1 px-2 py-4 sm:gap-2 sm:px-4 sm:py-6">
+    <nav className="flex max-w-full bg-slate-900 px-4 py-4 text-white sm:gap-2 sm:px-4 sm:py-6">
+      <ul className="flex flex-1 gap-1 ">
         <li
           className={`nav-li ${
             router.pathname === "/" ? "border-b-2 border-rose-700" : ""
@@ -22,6 +22,12 @@ function Navbar() {
           <Link href="/top-anime">Top Anime</Link>
         </li>
       </ul>
+      <form className="flex-1">
+        <input
+          type="text"
+          className="h-full w-full rounded-md border-2 border-rose-700 bg-slate-900"
+        />
+      </form>
     </nav>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
+import Hero from "../components/Hero"
+
 function Home() {
   return (
     <div className="m-auto min-h-screen bg-slate-900">
-      <h1 className="text-center text-2xl text-white">
-        Hii! Im here using Next.js! Yahaha
-      </h1>
+      <Hero />
     </div>
   )
 }
