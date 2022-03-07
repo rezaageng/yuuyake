204e2f86ed18eb855fbaa6f7ff965961bb3d7eab
Reza Ageng <rezaageng5@gmail.com>
2022-03-07 11:54:02 +0700

    update navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,16 +1,37 @@
 import Link from "next/link"
+import { useRouter } from "next/router"
 
 function Navbar() {
+  const router = useRouter()
+
   return (
-    <nav>
-      <ul className="flex p-2 text-center">
-        <li className="flex-1">
+    <nav className="max-w-full bg-slate-900 text-white">
+      <ul className="flex gap-1 px-2 py-4 sm:gap-2 sm:px-4 sm:py-6">
+        <li
+          className={`nav-li ${
+            router.pathname === "/"
+              ? "ring-2 ring-inset ring-rose-700 sm:ring"
+              : ""
+          }`}
+        >
           <Link href="/">Home</Link>
         </li>
-        <li className="flex-1">
+        <li
+          className={`nav-li ${
+            router.pathname === "/top-anime"
+              ? "ring-2 ring-inset ring-rose-700 sm:ring"
+              : ""
+          }`}
+        >
           <Link href="/top-anime">Top Anime</Link>
         </li>
-        <li className="flex-1">
+        <li
+          className={`nav-li ${
+            router.pathname === "/news"
+              ? "ring-2 ring-inset ring-rose-700 sm:ring"
+              : ""
+          }`}
+        >
           <Link href="/news">Anime News</Link>
         </li>
       </ul>
