18bda74868cac9d8f9b52d838485fe0309673471
Reza Ageng <rezaageng5@gmail.com>
2022-03-07 19:02:53 +0700

    add hero

---
+++ components/Hero.js
@@ -0,0 +1,17 @@
+function Hero() {
+  return (
+    <div className="mx-4 max-w-full">
+      <h1 className="text-4xl font-bold">Kawaii Neko</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias sint
+        animi possimus, repellat sit placeat dicta minus voluptate hic odit
+        tempore cupiditate explicabo, beatae sequi ullam, porro consectetur!
+        Nemo, illum velit. Fuga odit, quos iure vitae nulla nobis molestiae
+        reprehenderit reiciendis rem consequatur. Praesentium est nulla porro
+        sapiente nobis non.
+      </p>
+    </div>
+  )
+}
+
+export default Hero
