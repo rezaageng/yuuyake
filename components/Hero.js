19d43977b2f9b7925a5ae4ff9b89c60f9c063b30
Reza Ageng <rezaageng5@gmail.com>
2022-03-08 15:46:11 +0700

    update hero

--- components/Hero.js
+++ components/Hero.js
@@ -1,15 +1,11 @@
 function Hero() {
   return (
-    <div className="mx-4 max-w-full">
-      <h1 className="text-4xl font-bold">Kawaii Neko</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias sint
-        animi possimus, repellat sit placeat dicta minus voluptate hic odit
-        tempore cupiditate explicabo, beatae sequi ullam, porro consectetur!
-        Nemo, illum velit. Fuga odit, quos iure vitae nulla nobis molestiae
-        reprehenderit reiciendis rem consequatur. Praesentium est nulla porro
-        sapiente nobis non.
-      </p>
+    <div className="mx-4 flex h-52 max-w-full flex-col justify-end rounded-xl bg-hero bg-cover bg-center p-4 align-text-bottom md:h-80">
+      <div className="flex">
+        <h1 className="text-3xl font-bold md:text-5xl">Sunset/</h1>
+        <h1 className="self-end text-lg font-semibold md:text-2xl">夕焼け</h1>
+      </div>
+      <p className="md:text-lg">The sun is beautifull, isn&apos;t it?</p>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        hero: "url('/images/bg.png')",
+      },
+    },
   },
   plugins: [],
 }
