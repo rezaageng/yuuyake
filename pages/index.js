f408a0a5ed11592b5ed929be44707a8ead92ea87
Reza Ageng <rezaageng5@gmail.com>
2022-03-09 09:03:48 +0700

    add static site generator

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,22 @@
+import axios from "axios"
 import Hero from "../components/Hero"
 
-function Home() {
+function Home({ response }) {
   return (
     <div className="m-auto min-h-screen bg-slate-900">
       <Hero />
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const request = await axios.get("https://api.jikan.moe/v4/seasons/now")
+  const response = request.data
+
+  return {
+    props: { response },
+    revalidate: 10,
+  }
 }
 
 export default Home
