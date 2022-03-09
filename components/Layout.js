324aa21221638fe7192ec4d5fdd5d25e33e7b7f3
Reza Ageng <rezaageng5@gmail.com>
2022-03-09 13:14:45 +0700

    update navbar layout style

--- components/Layout.js
+++ components/Layout.js
@@ -3,8 +3,8 @@ import Navbar from "./Navbar"
 function Layout({ children }) {
   return (
     <div className="bg-slate-900 text-white">
+      <Navbar />
       <div className="mx-4 max-w-7xl xl:m-auto">
-        <Navbar />
         <main>{children}</main>
       </div>
     </div>
--- components/Navbar.js
+++ components/Navbar.js
@@ -5,31 +5,33 @@ function Navbar() {
   const router = useRouter()
 
   return (
-    <nav className="sticky top-0 z-50 flex max-w-full bg-slate-900/90 py-4 text-white backdrop-blur-xl sm:gap-2 sm:py-6">
-      <ul className="flex flex-1 gap-1 ">
-        <li
-          className={`nav-li ${
-            router.pathname === "/" ? "border-b-2 border-rose-700" : ""
-          }`}
-        >
-          <Link href="/">Home</Link>
-        </li>
-        <li
-          className={`nav-li ${
-            router.pathname === "/top-anime" ? "border-rose-700" : ""
-          }`}
-        >
-          <Link href="/top-anime">Top Anime</Link>
-        </li>
-      </ul>
-      <form className="flex flex-1 flex-col justify-center">
-        <input
-          type="text"
-          className="w-36 self-end rounded-md bg-slate-900 px-2 outline-none outline-offset-0 outline-rose-900 focus:outline-rose-700"
-          placeholder="Search anime..."
-        />
-      </form>
-    </nav>
+    <div className="sticky top-0 z-50 bg-slate-900/90 backdrop-blur-xl">
+      <nav className="mx-4 flex max-w-7xl py-4  text-white sm:gap-2  sm:py-6 xl:m-auto">
+        <ul className="flex flex-1 gap-1 ">
+          <li
+            className={`nav-li ${
+              router.pathname === "/" ? "border-b-2 border-rose-700" : ""
+            }`}
+          >
+            <Link href="/">Home</Link>
+          </li>
+          <li
+            className={`nav-li ${
+              router.pathname === "/top-anime" ? "border-rose-700" : ""
+            }`}
+          >
+            <Link href="/top-anime">Top Anime</Link>
+          </li>
+        </ul>
+        <form className="flex flex-1 flex-col justify-center">
+          <input
+            type="text"
+            className="w-36 self-end rounded-md bg-slate-900 px-2 outline-none outline-offset-0 outline-rose-900 focus:outline-rose-700"
+            placeholder="Search anime..."
+          />
+        </form>
+      </nav>
+    </div>
   )
 }
 
