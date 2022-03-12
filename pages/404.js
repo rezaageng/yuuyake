4dd33c879c1a10bf0004362b1af77b8171e152de
Reza Ageng <rezaageng5@gmail.com>
2022-03-12 19:46:02 +0700

    anime search not found

--- pages/404.js
+++ pages/404.js
@@ -1,19 +1,8 @@
 import Image from "next/image"
+import NotFound from "../components/NotFound"
 
-function NotFound() {
-  return (
-    <div className="mx-auto my-16 max-w-xs">
-      <Image
-        src="/images/404.png"
-        alt="Cover"
-        width={1271}
-        height={774}
-        layout="responsive"
-        objectFit="cover"
-        priority={true}
-      />
-    </div>
-  )
+function Custom404() {
+  return <NotFound />
 }
 
-export default NotFound
+export default Custom404
--- pages/search/index.js
+++ pages/search/index.js
@@ -3,6 +3,7 @@ import { useRouter } from "next/router"
 import { useContext } from "react"
 import Anime from "../../components/Anime"
 import AnimeSkeleton from "../../components/AnimeSkeleton"
+import NotFound from "../../components/NotFound"
 import { AppContext } from "../../context/app-context"
 
 function Search() {
@@ -14,7 +15,13 @@ function Search() {
       <Head>
         <title>{router.query.q} · Yuuyake</title>
       </Head>
-      {context.loading ? <AnimeSkeleton /> : <Anime anime={context} />}
+      {context.loading ? (
+        <AnimeSkeleton />
+      ) : !context.data.length ? (
+        <NotFound />
+      ) : (
+        <Anime anime={context} />
+      )}
     </>
   )
 }
