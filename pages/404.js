91841f1a88d4dab3f9e2c79983318939650b13e2
Reza Ageng <rezaageng5@gmail.com>
2022-03-12 19:53:40 +0700

    add 404 title

--- pages/404.js
+++ pages/404.js
@@ -1,8 +1,15 @@
-import Image from "next/image"
+import Head from "next/head"
 import NotFound from "../components/NotFound"
 
 function Custom404() {
-  return <NotFound />
+  return (
+    <>
+      <Head>
+        <title>404 Not Found</title>
+      </Head>
+      <NotFound />
+    </>
+  )
 }
 
 export default Custom404
