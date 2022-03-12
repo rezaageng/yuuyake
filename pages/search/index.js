6b4e9fc538db72c85ec0dccd6f6ac71595df3b2a
Reza Ageng <rezaageng5@gmail.com>
2022-03-12 17:29:37 +0700

    add head

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,22 +1,19 @@
+import Head from "next/head"
+import { useRouter } from "next/router"
 import { useContext } from "react"
 import Anime from "../../components/Anime"
 import { AppContext } from "../../context/app-context"
 
 function Search() {
   const context = useContext(AppContext)
-  console.log(context)
+  const router = useRouter()
 
   return (
     <>
-      {context.loading ? (
-        <>
-          <h1>Loading</h1>
-        </>
-      ) : (
-        <>
-          <Anime anime={context} />
-        </>
-      )}
+      <Head>
+        <title>{router.query.q} · Yuuyake</title>
+      </Head>
+      {context.loading ? <h1>Loading</h1> : <Anime anime={context} />}
     </>
   )
 }
